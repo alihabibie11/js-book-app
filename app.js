ae76626f1dbae218f9f32bc16d20652d1fa374ba
alihabibie11 <malizzulhaq08@gmail.com>
2021-01-02 15:19:33 +0700

    Change "X" button to "x" button..

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ class UI {
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
-      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">x</a></td>
     `;
 
     list.appendChild(row);
